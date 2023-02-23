6aef84e04df4b3df02e16f45510be10b6a185bde
Denis Tkachenko <tkachenko.den@gmail.com>
2023-02-23 21:27:19 +0300

    results of stage 1

--- SQLite Viewer/task/src/viewer/SQLiteViewer.java
+++ SQLite Viewer/task/src/viewer/SQLiteViewer.java
@@ -5,10 +5,25 @@ import javax.swing.*;
 public class SQLiteViewer extends JFrame {
 
     public SQLiteViewer() {
+        super("SQLite Viewer");
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setSize(700, 900);
         setLayout(null);
         setResizable(false);
         setLocationRelativeTo(null);
+
+        initControls();
+        setVisible(true);
+    }
+
+    void initControls() {
+        add(new JTextField() {{
+            setName("FileNameTextField");
+            setBounds(10, 10, 550, 30);
+        }});
+        add(new JButton("Open") {{
+            setName("OpenFileButton");
+            setBounds(570, 10, 100, 30);
+        }});
     }
 }
