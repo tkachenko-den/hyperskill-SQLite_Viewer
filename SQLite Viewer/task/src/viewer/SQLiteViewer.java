385fcdd808ff6d3decb70e9c91b89c750b4f6922
Denis Tkachenko <tkachenko.den@gmail.com>
2023-02-23 14:04:27 +0300

    initial commit

---
+++ SQLite Viewer/task/src/viewer/SQLiteViewer.java
@@ -0,0 +1,14 @@
+package viewer;
+
+import javax.swing.*;
+
+public class SQLiteViewer extends JFrame {
+
+    public SQLiteViewer() {
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setSize(700, 900);
+        setLayout(null);
+        setResizable(false);
+        setLocationRelativeTo(null);
+    }
+}
